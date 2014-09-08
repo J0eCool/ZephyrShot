45e2c297d472360954be39fa837628e3ce7bc250
J0eCool <count.j0ecool@gmail.com>
2014-09-07 23:26:28 -0700

    Add UI element

---
+++ Assets/Scripts/UI/BaseUiComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BaseUiComponent : MonoBehaviour {
+	private tk2dUIItem uiItem;
+
+	void Awake() {
+		uiItem = GetComponent<tk2dUIItem>();
+	}
+
+	void OnEnable() {
+		uiItem.OnClick += OnClick;
+	}
+
+	void OnDisable() {
+		uiItem.OnClick -= OnClick;
+	}
+
+	protected virtual void OnClick() { }
+}
---
+++ Assets/Scripts/UI/TestButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestButton : BaseUiComponent {
+	public string writeStr = "foo";
+	private int counter = 0;
+
+	protected override void OnClick() {
+		Debug.Log(writeStr + " : " + counter);
+		counter++;
+	}
+}
