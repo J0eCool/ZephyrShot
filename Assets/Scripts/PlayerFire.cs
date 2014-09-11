9331e41bdeb67c9e4f3f4d71f24b77bbd99f237d
J0eCool <count.j0ecool@gmail.com>
2014-09-11 00:51:06 -0700

    Add UnityTestTools, gitignore, enemy sprite

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -6,6 +6,8 @@ public class PlayerFire : MonoBehaviour {
 	public GameObject bulletPrefab;
 	public float fireRate = 2.5f;
 	public float bulletSpeed = 7.5f;
+	public int numBullets = 1;
+	public float spreadPerBullet = 5.0f;
 
 	private float timer = 0.0f;
 
@@ -14,14 +16,22 @@ public class PlayerFire : MonoBehaviour {
 		float timePerShot = 1.0f / fireRate;
 		if (timer >= timePerShot) {
 			timer -= timePerShot;
-			GameObject obj = GameObject.Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
-			obj.GetComponent<Bullet>().Init(new Vector3(0.0f, bulletSpeed, 0.0f));
 
-			GameObject bulletFolder = GameObject.Find("Bullets");
-			if (bulletFolder == null) {
-				bulletFolder = new GameObject("Bullets");
+			float totalAngle = numBullets * spreadPerBullet * Mathf.Deg2Rad;
+			float w = (numBullets - 1) / 2.0f;
+			for (int i = 0; i < numBullets; i++) {
+				float angle = (float)(i - w) / numBullets * totalAngle;
+				Vector3 vel = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0.0f) * bulletSpeed;
+
+				GameObject obj = GameObject.Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
+				obj.GetComponent<Bullet>().Init(vel);
+
+				GameObject bulletFolder = GameObject.Find("Bullets");
+				if (bulletFolder == null) {
+					bulletFolder = new GameObject("Bullets");
+				}
+				obj.transform.parent = bulletFolder.transform;
 			}
-			obj.transform.parent = bulletFolder.transform;
 		}
 	}
 }
---
+++ Assets/Scripts/UI/AdjustGunButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum UpgradeType {
+	FireRate,
+	BulletSpeed,
+	ShotNumber,
+}
+
+public class AdjustGunButton : BaseUiComponent {
+	public bool positive;
+	public UpgradeType type;
+	public float increment;
+
+	PlayerFire fire;
+
+	void Start() {
+		fire = GameObject.Find("Player").GetComponent<PlayerFire>();
+	}
+
+	protected override void OnClick() {
+		float amount = positive ? increment : -increment;
+
+		switch (type) {
+		case UpgradeType.BulletSpeed:
+			fire.bulletSpeed += amount;
+			break;
+		case UpgradeType.FireRate:
+			fire.fireRate += amount;
+			break;
+		case UpgradeType.ShotNumber:
+			fire.numBullets += (int)amount;
+			break;
+		}
+	}
+}
