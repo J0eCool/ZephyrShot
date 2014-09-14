ace86f2a30bfe9795b7da847bde44db21d32ae49
J0eCool <count.j0ecool@gmail.com>
2014-09-13 19:04:34 -0700

    Bullets use a more accurate culling function

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class GetsShot : MonoBehaviour {
+public class Health : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other) {
         Bullet b = other.gameObject.GetComponent<Bullet>();
         if (b != null) {
---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HealthBar : MonoBehaviour {
+	public tk2dSlicedSprite sprite;
+
+	void Update() {
+	}
+}
