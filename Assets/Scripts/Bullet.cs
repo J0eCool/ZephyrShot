9010e9f1f40f1a231f3932b139454bd0c93d7c5c
J0eCool <count.j0ecool@gmail.com>
2014-09-14 01:53:00 -0700

    Move camera culling logic out to its own component

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,23 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Bullet : MonoBehaviour {
-	private static Vector3 screenScaleVec = new Vector3(1.0f / Screen.width, 1.0f / Screen.height);
-	private const float kScreenBoundary = 0.05f;
-
 	private Vector3 velocity;
 
 	public void Init(Vector3 vel) {
 		velocity = vel;
+		float angle = Mathf.Atan2(vel.y, vel.x);
+		transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg - 90.0f, Vector3.forward);
 	}
 
 	void Update() {
 		transform.position += velocity * Time.deltaTime;
-
-		Vector3 point = Camera.main.WorldToScreenPoint(transform.position);
-		point.Scale(screenScaleVec);
-		if (point.x < -kScreenBoundary || point.x > 1.0f + kScreenBoundary
-				|| point.y < -kScreenBoundary || point.y > 1.0f + kScreenBoundary) {
-			GameObject.Destroy(gameObject);
-		}
 	}
 }
---
+++ Assets/Scripts/RemoveWhenLeavingScreen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RemoveWhenLeavingScreen : MonoBehaviour {
+	private readonly static Vector3 screenScaleVec = new Vector3(1.0f / Screen.width, 1.0f / Screen.height);
+	private const float kScreenBoundary = 0.05f;
+	private const float kSafeScreenBoundary = 1.5f;
+
+	private bool didEnterScreen = false;
+
+	void Update() {
+		Vector3 point = Camera.main.WorldToScreenPoint(transform.position);
+		point.Scale(screenScaleVec);
+		
+		float destroyBoundary = kScreenBoundary;
+		if (!didEnterScreen) {
+			if (IsWithinBorder(point, kScreenBoundary)) {
+				didEnterScreen = true;
+			}
+			destroyBoundary = kSafeScreenBoundary;
+		}
+
+		if (!IsWithinBorder(point, destroyBoundary)) {
+			GameObject.Destroy(gameObject);
+		}
+	}
+
+	private static bool IsWithinBorder(Vector3 point, float boundary) {
+		return point.x > -boundary && point.x < 1.0f + boundary
+			&& point.y > -boundary && point.y < 1.0f + boundary;
+	}
+}
