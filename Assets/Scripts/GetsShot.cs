e7fca411722b4c77522d7902d792ae66ef49db3f
Jacob Gravelle <jacob@pocketgems.com>
2014-09-13 18:19:23 -0700

    Remove TestTools, enemy receives bullet hits

---
+++ Assets/Scripts/GetsShot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GetsShot : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D other) {
+        Bullet b = other.gameObject.GetComponent<Bullet>();
+        if (b != null) {
+            GameObject.Destroy(b.gameObject);
+        }
+    }
+}
