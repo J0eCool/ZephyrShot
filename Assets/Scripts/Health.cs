0f73b6a5a0ea366582fa5a5421a5264c5311901f
J0eCool <count.j0ecool@gmail.com>
2014-09-14 17:59:18 -0700

    Enemy health bars

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -27,4 +27,8 @@ public class Health : MonoBehaviour {
 			}
         }
     }
+
+	public float GetHealthFraction() {
+		return (float)health / maxHealth;
+	}
 }
--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -4,7 +4,33 @@ using System.Collections.Generic;
 
 public class HealthBar : MonoBehaviour {
 	public tk2dSlicedSprite sprite;
+	public Health health;
+	public bool hideWhenFull = true;
+
+	private float baseWidth;
+	private float cachedHealth = -1.0f;
+
+	void Start() {
+		baseWidth = sprite.dimensions.x;
+	}
 
 	void Update() {
+		float t = health.GetHealthFraction();
+		if (t != cachedHealth) {
+			cachedHealth = t;
+			if (hideWhenFull && t >= 1.0f) {
+				SetChildrenActive(false);
+			}
+			else {
+				SetChildrenActive(true);
+				sprite.dimensions = new Vector2(baseWidth * t, sprite.dimensions.y);
+			}
+		}
+	}
+
+	void SetChildrenActive(bool active) {
+		for (int i = 0; i < transform.childCount; i++) {
+			transform.GetChild(i).gameObject.SetActive(active);
+		}
 	}
 }
